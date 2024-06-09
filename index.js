b387061b0a56705eadc5a2483af3cabe293fa156
Rahul Sharma <44360511+sharma03r@users.noreply.github.com>
2024-06-09 10:53:50 +0530

    Added origin for frontend callls

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://pwd-gen-six.vercel.app/" }));
 app.listen(process.env.PORT, () => {
   console.log("Server running on port: ", process.env.PORT);
 });
